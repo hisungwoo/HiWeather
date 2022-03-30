9ebe101309c45dadbcbe4e898196394be0211d34
hisun <hisungwoo007@naver.com>
2022-03-30 11:13:33 +0900

    mvp 패턴 변경중

--- app/src/main/java/org/ganache/hiweather/view/main/MainContract.java
+++ app/src/main/java/org/ganache/hiweather/view/main/MainContract.java
@@ -1,14 +1,34 @@
 package org.ganache.hiweather.view.main;
 
+import android.content.Context;
+
+import org.ganache.hiweather.model.LatXLngY;
+import org.ganache.hiweather.model.TomorrowWeather;
+
+import java.util.HashMap;
+import java.util.List;
+
 public interface MainContract {
     interface View{
-        void showResult(int answer);      //값을 보여줄 View 메소드 선언
+        void startApp();
         void showProgress(boolean isShow);
-        void setNowData(String nowDay, String nowTime);
+        void setNowData(String nowDay, String nowTime); // 현재 날짜, 시간 셋팅
         void setNowLayout(int layoutColor, int StatusColor);    // 현재 시간에 맞춰 레이아웃 디자인 변경
+        void setCoordinate(double latitude, double longitude, LatXLngY gridXy); // 현재 좌표 셋팅
+        void getWeather();  // 현재 날짜, 시간, 좌표를 이용하여 날씨 정보 획득
+        void showToast(String text);
+        void updateRecyclerView(List<TomorrowWeather> list); // 시간별 날씨 정보 RecyclerView 업데이트
+        void setWeatherImg(int drawable);   // 날씨 이미지 셋팅
+        void setWeatherTextView(String weather);    // 날씨 정보 셋팅
+        void setWeatherItems(HashMap<String, String> items);    // 습도, 강수확률, 강수량, 풍속 등 셋팅
+        void setLocationTextView(String sido, String gugun);    // 현재 내 위치 시군구 셋팅
+        void setMyLocation();   // 현재 나의 위치 획득
+        void startAnim();
     }
     interface Presenter{
-        void addNum(int num1, int num2);  //결과 값 구하기 위한 메소드 선언
-        void getNowData();
+        void getNowData();  // 현재 날짜,시간 정보 획득
+        void getCoordinate(Context context);  // 현재 위치의 좌표 획득
+        void getWeatherInfo(String nowDay, String nowTime, LatXLngY gridXy); // 날짜, 시간, 좌표를 이용하여 날씨 정보 획득
+        void getMyLocation(Context cont, double latitude, double longitude);    // 현재 나의 위치
     }
 }
